0b2f091ceeaf1e5eeec90a1b80f553cd0af72faa
Eric-Paul Lecluse <e@epologee.com>
2014-04-16 10:47:34 +0200

    Cleanup
    Removed the `bundle install` command, causes issues in sudo-requiring environments

--- iPhoneProject/post_klipp_create.sh
+++ iPhoneProject/post_klipp_create.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
 
 set -e
-
 rm post_klipp_create.sh
 
-# wldir=`pwd`
-# cd ../../
-# 
-# git submodule update --init --recursive
-# 
-# cd "$wldir"
-
 cd XXPRODUCT_NAMEXX
-bundle install
 chmod +x run_command_line_unit_tests.sh
 pod install
 ./run_command_line_unit_tests.sh
 cd ..
-
-# x=`/usr/bin/osascript <<EOT
-# tell application "Finder"
-#   activate
-#   set myReply to button returned of (display dialog "Open workspace in Xcode?")
-# end tell
-# EOT`
-# if [[ $x = "OK" ]]; then 
-#   open XXPRODUCT_NAMEXX/XXPRODUCT_NAMEXX.xcworkspace
-# fi
-# 
-# echo ""
